8ad888e5eca23ee7634385833ad09599a89dd284
Ken Ho <ken.ho@hotmail.co.uk>
2016-01-26 16:12:28 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instapic::Application.config.secret_key_base = 'cb0b0b52ea5776ca325237a2b3f9eebb868351c7e74b2045b93ae798511881c8bc19bb5211f449de4643883cf561d6a15218c6e0d59363bc309e4c5b78cfa291'
